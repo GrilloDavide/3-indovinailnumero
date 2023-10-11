34ab21efe4bab54406f111396452602e934ce52a
Wawewu <ginoilritorno@gmail.com>
2023-10-11 18:30:56 +0200

    implementato il server con i thread

---
+++ src/main/java/it/fi/meucci/Client2.java
@@ -0,0 +1,38 @@
+package it.fi.meucci;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client2 {
+    public static void main( String[] args ) throws IOException {
+
+        Socket mioSocket = new Socket( InetAddress.getLocalHost() , 6789);
+
+        DataOutputStream outServer = new DataOutputStream(mioSocket.getOutputStream());
+        Scanner inServer = new Scanner (new InputStreamReader(mioSocket.getInputStream()));
+
+        System.out.println( " Connessione effettuata " );
+
+        Scanner inputTastiera = new Scanner(System.in);
+        String outputString = " ";
+        String inputString = " ";
+
+        System.out.println("[SERVER] "+inServer.nextLine());
+        System.out.println("[SERVER] "+inServer.nextLine());
+
+        do{
+
+            System.out.print("[TU] ");outputString = inputTastiera.nextLine();
+            outServer.writeBytes(outputString + "\n");
+            inputString = inServer.nextLine();
+            System.out.println("[SERVER] "+inputString);
+
+        }while((!inputString.equals("Hai indovinato!")));
+
+        mioSocket.close();
+        inputTastiera.close();
+    }
+
+}
---
+++ src/main/java/it/fi/meucci/ServerThread.java
@@ -0,0 +1,56 @@
+package it.fi.meucci;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThread extends Thread{
+
+    ServerSocket server;
+    Socket client;
+    BufferedReader inClient;
+    DataOutputStream outClient;
+
+    public ServerThread(Socket client) throws IOException {
+        this.client = client;
+        inClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        outClient = new DataOutputStream(client.getOutputStream());
+    }
+
+    @Override
+    public void run() {
+        try{
+           startComm();
+        }catch (Exception e){
+            System.out.println("Skill issue");
+            e.printStackTrace();
+        }
+    }
+
+    public void startComm() throws IOException {
+
+        int inputNumber;
+        int randomNumber = (int)(Math.random() * 1000);
+            outClient.writeBytes("--INDOVINA IL NUMERO--\n");
+            //outClient.writeBytes("  inserire un numero da 1 a 1000"+" il numero e "+randomNumber+"\n"); utilizzato in debug
+        outClient.writeBytes("  inserire un numero da 1 a 1000\n");
+        for(;;){
+            inputNumber = Integer.parseInt(inClient.readLine());
+
+            System.out.println("[CLIENT] "+ randomNumber);
+            if(inputNumber == randomNumber ){
+                outClient.writeBytes("Hai indovinato!\n");
+                break;
+            }else{
+                outClient.writeBytes("Sbagliato, riprova!\n");
+            }
+
+        }
+
+        System.out.println("Termine connessione con "+client.getLocalAddress());
+        client.close();
+    }
+}
